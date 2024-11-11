42119ef47773a5438cb2b905de9ed130c8509f1f
miro <jarbasai@mailfence.com>
2024-11-11 03:40:28 +0000

    modernize

--- __init__.py
+++ __init__.py
@@ -1,150 +1,132 @@
-from mycroft import MycroftSkill, intent_handler
-from mycroft.messagebus.message import Message
+from ovos_bus_client.message import Message
+from ovos_color_parser import sRGBAColor, color_from_description, lookup_name
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills.ovos import OVOSSkill
 
 
-from .colors import (
-    Color,
-    ColorFactory,
-    is_hex_code_valid,
-    is_rgb_value_valid,
-)
-
-
-class ColorPicker(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
-        self.color_factory = None
-        self.color_name_to_hex_map = dict()
-        self.colors_by_hex = dict()
-
-    def initialize(self):
-        self.color_name_to_hex_map = self.translate_namedvalues("color-names")
-        self.color_factory = ColorFactory(self.color_name_to_hex_map)
+class ColorPicker(OVOSSkill):
 
     @intent_handler("request-color.intent")
-    def handle_request_color(self, message):
+    def handle_request_color(self, message: Message):
         """Handle requests for color where the color format is unknown.
 
         Example: 'What color is _________'
         """
         requested_color = message.data.get("requested_color")
 
-        is_color_name = self.color_name_to_hex_map.get(requested_color) is not None
-        if is_color_name:
-            mock_message = Message("", {"color": requested_color})
-            self.handle_request_color_by_name(mock_message)
+        try:
+            color = sRGBAColor.from_hex_str(requested_color.replace(" ", ""))
+            message = message.forward("", {"hex_code": requested_color})
+            self.handle_request_color_by_hex(message)
+            return
+        except:  # not a hex  code
+            pass
 
-        is_hex_code = is_hex_code_valid(requested_color.replace(" ", ""))
-        if is_hex_code:
-            mock_message = Message("", {"hex_code": requested_color})
-            self.handle_request_color_by_hex(mock_message)
+        try:
+            r, g, b = requested_color.split()
+            message = message.forward("", {"rgb": requested_color})
+            self.handle_request_color_by_rgb(message)
+            return
+        except:  # not rgb
+            pass
 
-        is_rgb_value = is_rgb_value_valid(requested_color)
-        if is_rgb_value:
-            mock_message = Message("", {"rgb": requested_color})
-            self.handle_request_color_by_rgb(mock_message)
+        message = message.forward("", {"color": requested_color})
+        self.handle_request_color_by_name(message)
 
     @intent_handler("request-color-by-name.intent")
-    def handle_request_color_by_name(self, message):
+    def handle_request_color_by_name(self, message: Message):
         """Handle named color requests.
 
         Example: 'Show me the color burly wood'
         """
         requested_color = message.data.get("color")
         self.log.info("Requested color: %s", requested_color)
-        try:
-            color = self.color_factory.create("name", requested_color)
-        except ValueError:
-            self.speak_dialog("color-not-found")
-            return
+        color = color_from_description(requested_color, lang=self.lang.split("-")[0])
 
         self.speak_dialog(
             "report-color-by-name",
             data={
                 "color_name": color.name,
-                "hex_code": color.speakable_hex_code,
-                "red_value": color.rgb_values.red,
-                "green_value": color.rgb_values.green,
-                "blue_value": color.rgb_values.blue,
+                "hex_code": color.hex_str,
+                "red_value": color.r,
+                "green_value": color.g,
+                "blue_value": color.b,
             },
         )
         self.display_single_color(color)
 
     @intent_handler("request-color-by-hex.intent")
-    def handle_request_color_by_hex(self, message):
+    def handle_request_color_by_hex(self, message: Message):
         """Handle named color requests.
 
         Example: 'what color has a hex code of bada55'
         """
         requested_hex_code = message.data.get("hex_code").replace(" ", "")
         self.log.info("Requested color: %s", requested_hex_code)
+        color = sRGBAColor.from_hex_str(requested_hex_code)
         try:
-            color = self.color_factory.create("hex", requested_hex_code)
+            color.name = lookup_name(color, lang=self.lang.split("-")[0])
         except ValueError:
-            self.speak_dialog("color-not-found")
-            return
+            # color is not named
+            pass
 
-        color_dialog_data = {
-            "color_name": color.name,
-            "hex_code": color.speakable_hex_code,
-            "red_value": color.rgb_values.red,
-            "green_value": color.rgb_values.green,
-            "blue_value": color.rgb_values.blue,
-        }
         if color.name:
             self.speak_dialog(
                 "report-color-by-hex-name-known",
-                data=color_dialog_data,
+                data={
+                    "color_name": color.name,
+                    "red_value": color.r,
+                    "green_value": color.g,
+                    "blue_value": color.b,
+                }
             )
         else:
             self.speak_dialog(
                 "report-color-by-hex-name-not-known",
-                data=color_dialog_data,
+                data={
+                    "red_value": color.r,
+                    "green_value": color.g,
+                    "blue_value": color.b,
+                }
             )
 
     @intent_handler("request-color-by-rgb.intent")
-    def handle_request_color_by_rgb(self, message):
+    def handle_request_color_by_rgb(self, message: Message):
         """
         Handle RGB color requests
 
         Example: what color has the RGB value of 172 172 172
         """
-        rgb_string = message.data.get("rgb").split()
+        r, g, b = message.data["rgb"].split()
+        color = sRGBAColor(r, g, b)
 
-        # TODO this should be handled in the ColorFactory
-        # TODO need to decide on a single RGB data type and use it consistently.
-        if is_rgb_value_valid(rgb_string):
-            requested_rgb = tuple([int(value) for value in rgb_string])
         try:
-            color = self.color_factory.create("rgb", requested_rgb)
+            color.name = lookup_name(color, lang=self.lang.split("-")[0])
         except ValueError:
-            self.speak_dialog("color-not-found")
-            return
-
-        color_dialog_data = {
-            "color_name": color.name,
-            "hex_code": color.speakable_hex_code,
-        }
+            # color is not named
+            pass
 
         if color.name is None:
             self.speak_dialog(
-                "report-color-by-rgb-name-known",
-                data=color_dialog_data,
+                "report-color-by-rgb-name-not-known",
+                data={"red_value": color.r,
+                      "green_value": color.g,
+                      "blue_value": color.b}
             )
         else:
+            speakable_hex_code = color.hex_str  # TODO
             self.speak_dialog(
-                "report-color-by-rgb-name-not-known",
-                data=color_dialog_data,
+                "report-color-by-rgb-name-known",
+                data={
+                    "color_name": color.name,
+                    "hex_code": speakable_hex_code
+                }
             )
 
-    def display_single_color(self, color: Color):
+    def display_single_color(self, color: sRGBAColor):
         """Display details of a single color"""
         self.gui["colorName"] = color.name.title()
-        self.gui["colorHex"] = color.hex_code.upper()
-        self.gui["colorRGB"] = f"RGB: {str(color.rgb_values)}"
-        self.gui["textColor"] = color.contrasting_tone
+        self.gui["colorHex"] = color.hex_str.upper()
+        self.gui["colorRGB"] = f"RGB: {color.r}, {color.g}, {color.b}"
+        self.gui["textColor"] = color.description
         self.gui.show_page("single-color.qml")
-
-
-def create_skill():
-    return ColorPicker()
